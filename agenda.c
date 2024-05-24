deaafe4079e148fd671083a2ccc0fd341cf70d22
sbaecker2005 <144857264+sbaecker2005@users.noreply.github.com>
2024-05-24 20:01:40 -0300

    Update agenda.c

--- agenda.c
+++ agenda.c
@@ -14,3 +14,22 @@ void adicionarContato(Agenda *agenda, int tipoContato) {
         contatoArray = agenda->contatosTrabalho;
         quantidade = &agenda->quantidadeTrabalho;
     }
+
+    if (*quantidade >= MAX_CONTATOS) {
+        printf("A lista de contatos esta cheia!\n");
+        return;
+    }
+
+    Contato novoContato;
+    printf("\nAdicionando Contato:\n");
+    printf("Nome: ");
+    scanf("%s", novoContato.nome);
+    printf("Telefone: ");
+    scanf("%s", novoContato.telefone);
+    printf("Email: ");
+    scanf("%s", novoContato.email);
+
+    if (strstr(novoContato.email, "@") == NULL) {
+        printf("Email invalido! O email deve conter '@'.\n");
+        return;
+    }
