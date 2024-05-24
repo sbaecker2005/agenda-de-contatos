565ce586c0aeb1814f8e0c05932e0712df74defc
Matheus Concon <142353640+goldjkk@users.noreply.github.com>
2024-05-24 20:07:37 -0300

    Update agenda.h

--- agenda.h
+++ agenda.h
@@ -1,17 +1,20 @@
-#ifndef CLIENTE_BANCO_H
-#define CLIENTE_BANCO_H
+#ifndef AGENDA_H
+#define AGENDA_H
 
-struct Cliente {
-    char nome[100];
-    char cpf[12];
-    int tipo;
-    double saldo;
-    char senha[20];
-};
-struct Extrato {
-    char cpf[12];
-    char data[20];
-    double valor;
-    double tarifa;
-    double saldo;
-};
+#define MAX_CONTATOS 255
+#define MAX_NOME 50
+#define MAX_TELEFONE 15
+#define MAX_EMAIL 50
+
+typedef struct {
+    char nome[MAX_NOME];
+    char telefone[MAX_TELEFONE];
+    char email[MAX_EMAIL];
+} Contato;
+
+typedef struct {
+    Contato contatosPessoais[MAX_CONTATOS];
+    Contato contatosTrabalho[MAX_CONTATOS];
+    int quantidadePessoais;
+    int quantidadeTrabalho;
+} Agenda;
